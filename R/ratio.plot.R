1f8dec6204c8b12035754b034c7a78a0f924df6b
Jeff Gentry <jgentry@jimmy.harvard.edu>
2004-07-15 19:20:52 +0000

    updating the class checking to inherits
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rama@9432 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ratio.plot.R
+++ R/ratio.plot.R
@@ -1,13 +1,13 @@
 ratio.plot<-function(mcmc.obj,col=1,pch=1)
   {
-    if(class(mcmc.obj)!="mcmc")
-      stop("'mcmc.obj' should be of type 'mcmc'" , call. = TRUE)
-    
-    
+      if (!inherits(mcmc.obj, "mcmc"))
+          stop("'mcmc.obj' should be of type 'mcmc'" , call. = TRUE)
+
+
     ### Estimate the gene effects in sample 1
     x1<-mat.mean(mcmc.obj$gamma1)[,1]
     x2<-mat.mean(mcmc.obj$gamma2)[,1]
 
     plot((x1+x2)/2,x1-x2,pch=pch,col=col,xlab="Overall intensity (log2)",ylab="Log ratio (log2)")
-    
+
   }
